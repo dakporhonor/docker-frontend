c8a4fc968b00bfd6a4d0c58d2f3571d45b848f36
Daniel Akporhonor <daniel.akporhonor@axual.com>
2024-03-30 21:18:19 +0100

    implement docker and CI with react

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,12 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const txt = screen.getByText(/Hi there! let's become better all round/i);
+  expect(txt).toBeInTheDocument();
+
+  const txt2 = screen.getByText(/Hi there! let's become better all round/i);
+  expect(txt2).toBeInTheDocument();
+  
+  const txt3 = screen.getByText(/Hi there! let's become better all round/i);
+  expect(txt3).toBeInTheDocument();
 });
